2e815ad45503f30e61093fb21e96533943ba2bce
Roman Tsymbaliuk <rtsymbaliuk@deloitte.com>
2018-02-20 16:33:28 +0200

    Added DateTime (Date with TimeZone.)

---
+++ furniture/furniture/DateTime.swift
@@ -0,0 +1,14 @@
+//
+//  DateTime.swift
+//  furniture
+//
+//  Created by Roman Tsymbaliuk on 2/20/18.
+//  Copyright © 2018 Roman Tsymbaliuk. All rights reserved.
+//
+
+import Foundation
+
+public struct DateTime {
+   let date: Date
+   let timeZone: TimeZone
+}
