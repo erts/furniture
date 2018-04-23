577aff7e3aa5f38c334286da1af9debbdd819dc1
Roman.Tsymbaliuk <mail.morro@gmail.com>
2018-04-23 17:14:46 +0300

    Add Image Convertable protocol

---
+++ furniture/furniture/ImageConvertableProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  ImageConvertableProtocol.swift
+//  furniture
+//
+//  Created by Roman Tsymbaliuk on 4/16/18.
+//  Copyright © 2018 Roman Tsymbaliuk. All rights reserved.
+//
+
+import Foundation
+
+public protocol ImageConvertable {
+    var imageName: String? { get }
+}
